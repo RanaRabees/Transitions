dfc91a057c634a25a13e07c796ef6d868b69c46e
RanaRabees <104692519+RanaRabees@users.noreply.github.com>
2023-08-27 16:47:29 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import MyMenuDropdown from "./components/MyMenuDropdown"
+import MyListboxSelect from "./components/MyListboxSelect"
+import MyComboboxAutocomplete from "./components/MyComboboxAutocomplete"
+import MySwitchToggle from "./components/MySwitchToggle"
+import MyDisclosure from "./components/MyDisclosure"
+import MyDialogModal from "./components/MyDialogModal"
+import MyPopover from "./components/MyPopover"
+import MyRadioGroup from "./components/MyRadioGroup"
+import MyTabs from "./components/MyTabs"
+import MyTransition from "./components/MyTransition"
+
+export default function Home() {
+  return (
+    <>
+      <MyMenuDropdown />
+      <MyListboxSelect />
+      <MyComboboxAutocomplete />
+      <MySwitchToggle />
+      <MyDisclosure />
+      <MyDialogModal />
+      <MyPopover />
+      <MyRadioGroup />
+      <MyTabs />
+      <MyTransition />
+    </>
+  )
+}
